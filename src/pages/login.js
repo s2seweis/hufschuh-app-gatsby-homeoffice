df66d1d2bed6495f2708467cc73acf3d31f9c299
Sebastian Hans Weissenborn <weissenborn24seb@gmail.com>
2024-06-27 14:43:47 +0200

    callback code for google oauth

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Layout from "../components/Layout";
-import { login } from "../services/auth";
+// import { login } from "../services/auth";
 import { AuthBox } from "../components/AuthBox";
 
 export default function LoginPage() {
+  
   return (
     <Layout>
       <AuthBox mode={"login"} />
